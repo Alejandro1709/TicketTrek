546f9c8fd3676532619ff553f1ab60bbc99ba86c
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-01-20 16:17:48 -0500

    Change: Some small details in form and showcase

--- app/components/Showcase.tsx
+++ app/components/Showcase.tsx
@@ -6,9 +6,13 @@ function Showcase({ heading }: ShowcaseProps) {
   return (
     <section className='flex flex-col gap-4 lg:mt-16'>
       {heading ? <h2 className='text-2xl font-medium'>{heading}</h2> : null}
-      <ul className='flex flex-col gap-2'>
-        <li className='bg-slate-300 h-[129px]'>LI</li>
-        <li className='bg-slate-300 h-[129px]'>LI</li>
+      <ul className='flex flex-col lg:grid lg:grid-cols-3 gap-4 lg:gap-6'>
+        <li className='bg-slate-100 border h-[129px] rounded'>LI</li>
+        <li className='bg-slate-100 border h-[129px] rounded'>LI</li>
+        <li className='bg-slate-100 border h-[129px] rounded'>LI</li>
+        <li className='bg-slate-100 border h-[129px] rounded'>LI</li>
+        <li className='bg-slate-100 border h-[129px] rounded'>LI</li>
+        <li className='bg-slate-100 border h-[129px] rounded'>LI</li>
       </ul>
     </section>
   );
