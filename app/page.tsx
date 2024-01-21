ee60ec3fab3577b70b8c27c2de4e992e357ea6e2
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-01-21 12:11:17 -0500

    Move: root layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
 import Showcase from './components/Showcase';
 import TravelForm from './components/TravelForm';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen flex-col transition-all'>
-      <Navbar />
-      <Hero />
-      <main className='flex flex-col m-4 lg:mx-40'>
-        <TravelForm />
-        <Showcase heading='Los más populares' />
-      </main>
-    </div>
+    <>
+      <TravelForm />
+      <Showcase heading='Los destinos más populares' />
+    </>
   );
 }
