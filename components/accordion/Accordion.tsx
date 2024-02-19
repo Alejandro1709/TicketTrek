a8ac4208aa783e3422be87e482c4c222c546bb8f
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-02-19 17:45:06 -0500

    Add: Passengers Components in accordion

--- components/accordion/Accordion.tsx
+++ components/accordion/Accordion.tsx
@@ -1,47 +1,37 @@
-"use client";
+'use client';
 
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
-import DatePickerDemo from "../date-picker";
-import { useDateStore } from "@/stores/date-store";
+} from '@/components/ui/accordion';
+import DateRangePicker from '../range-picker';
+import { Button } from '../ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { PassengerList } from '../passenger';
 
 export default function AccordionDemo() {
-  const selectedFromDate = useDateStore((state) => state.selectedFromDate);
-  const selectedToDate = useDateStore((state) => state.selectedToDate);
-  const changeSelectedFromDate = useDateStore(
-    (state) => state.changeSelectedFromDate
-  );
-  const changeSelectedtoDate = useDateStore(
-    (state) => state.changeSelectedToDate
-  );
-
   return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
+    <Accordion type='single' collapsible className='w-full'>
+      <AccordionItem value='item-1'>
         <AccordionTrigger>Fechas del viaje</AccordionTrigger>
-        <AccordionContent className="flex flex-row gap-3">
-          <DatePickerDemo
-            date={selectedFromDate}
-            onDateChange={changeSelectedFromDate}
-          />
-          <DatePickerDemo
-            date={selectedToDate}
-            onDateChange={changeSelectedtoDate}
-          />
+        <AccordionContent className='flex flex-row gap-3'>
+          <DateRangePicker />
+          <Button className='w-full'>Apply</Button>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-2">
+      <AccordionItem value='item-2'>
         <AccordionTrigger>Pasajeros</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
+        <AccordionContent className='flex flex-col gap-3'>
+          <PassengerList />
+
+          <Button className='w-full' variant='outline'>
+            Add Passenger
+          </Button>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-3">
+      <AccordionItem value='item-3'>
         <AccordionTrigger>Asientos</AccordionTrigger>
         <AccordionContent>
           Yes. It&apos;s animated by default, but you can disable it if you
