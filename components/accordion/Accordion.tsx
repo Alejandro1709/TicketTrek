2cd94b72db3b54d83fde853600e61bf8865eee61
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-02-19 21:59:22 -0500

    Add: Passenger dialog

--- components/accordion/Accordion.tsx
+++ components/accordion/Accordion.tsx
@@ -6,10 +6,28 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import DateRangePicker from '../range-picker';
 import { Button } from '../ui/button';
 import { PassengerList } from '../passenger';
 import { Card, CardContent, CardHeader } from '../ui/card';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
 
 const seats = [
   'A1',
@@ -40,9 +58,87 @@ export default function AccordionDemo() {
         <AccordionContent className='flex flex-col gap-3'>
           <PassengerList />
 
-          <Button className='w-full' variant='outline'>
-            Add Passenger
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant='outline'>Añadir Pasajero</Button>
+            </DialogTrigger>
+            <DialogContent className='sm:max-w-[425px]'>
+              <DialogHeader>
+                <DialogTitle>Añadir Pasajero</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className='grid gap-4 py-4'>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label htmlFor='name' className='text-right'>
+                    Nombre
+                  </Label>
+                  <Input id='name' value='Pedro' className='col-span-3' />
+                </div>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label htmlFor='lastName' className='text-right'>
+                    Apellido
+                  </Label>
+                  <Input id='lastName' value='Duarte' className='col-span-3' />
+                </div>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label htmlFor='dni' className='text-right'>
+                    DNI
+                  </Label>
+                  <Input id='dni' value='80347367' className='col-span-3' />
+                </div>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label htmlFor='email' className='text-right'>
+                    Email
+                  </Label>
+                  <Input
+                    id='email'
+                    value='pedro.d@gmail.com'
+                    className='col-span-3'
+                  />
+                </div>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label htmlFor='address' className='text-right'>
+                    Dirección
+                  </Label>
+                  <Input
+                    id='address'
+                    value='Av. No Seas Sapo 420'
+                    className='col-span-3'
+                  />
+                </div>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label htmlFor='gender' className='text-right'>
+                    Género
+                  </Label>
+                  <Select>
+                    <SelectTrigger className='w-[180px]'>
+                      <SelectValue placeholder='SELECT' />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value='male'>Hombre</SelectItem>
+                      <SelectItem value='female'>Mujer</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label htmlFor='address' className='text-right'>
+                    Fecha. Nac
+                  </Label>
+                  <Input
+                    id='address'
+                    value='Av. No Seas Sapo 420'
+                    className='col-span-3'
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type='submit'>Añadir</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value='item-3'>
